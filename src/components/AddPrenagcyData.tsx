801d831e587941ab3d870fd0c48d58062dabd8ed
AbdullahFathan <abdullahfathan013@gmail.com>
2023-08-08 15:01:50 +0700

    style component done

--- src/components/AddPrenagcyData.tsx
+++ src/components/AddPrenagcyData.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import Arrow from "../assets/Arrow";
+
 const AddPrenagcyData = () => {
   return (
     <section className=" mt-[35px] ">
@@ -15,6 +18,50 @@ const AddPrenagcyData = () => {
         </h3>
 
         {/* Data Kehamilan Section */}
+        <h1 className="font-semibold text-2xl mt-9">Data Kehamilan</h1>
+
+        <button
+          type="button"
+          className="my-5 bg-orange text-white rounded-lg block text-sm font-semibold w-[60%] h-11"
+        >
+          Tambahkan Data Kehamilan
+        </button>
+
+        <div className="flex justify-between flex-row w-[60%] h-[70px] my-5 border border-border-grey border-solid rounded-lg py-4 px-6  ">
+          <div className="flex flex-col">
+            <h2 className="font-semibold ">Pemeriksaan - 3</h2>
+            <h4>
+              Status Kehamilan: <span className="text-green">Baik</span>
+            </h4>
+          </div>
+          <Link to={"/prenagcyData"} className="p-1">
+            <Arrow />
+          </Link>
+        </div>
+
+        <div className="flex justify-between flex-row w-[60%] h-[70px] my-5 border border-border-grey border-solid rounded-lg py-4 px-6  ">
+          <div className="flex flex-col">
+            <h2 className="font-semibold ">Pemeriksaan - 2</h2>
+            <h4>
+              Status Kehamilan: <span className="text-yellow">Lemah</span>
+            </h4>
+          </div>
+          <Link to={"/prenagcyData"} className="p-1">
+            <Arrow />
+          </Link>
+        </div>
+
+        <div className="flex justify-between flex-row w-[60%] h-[70px] my-5 border border-border-grey border-solid rounded-lg py-4 px-6  ">
+          <div className="flex flex-col">
+            <h2 className="font-semibold ">Pemeriksaan - 2</h2>
+            <h4>
+              Status Kehamilan: <span className="text-red">Beresiko</span>
+            </h4>
+          </div>
+          <Link to={"/prenagcyData"} className="p-1">
+            <Arrow />
+          </Link>
+        </div>
       </div>
     </section>
   );
