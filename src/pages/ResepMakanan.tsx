1364373ddf4aff6a30f578630f6f8b1ec30a234e
AbdullahFathan <abdullahfathan013@gmail.com>
2023-08-15 11:02:36 +0700

    style: add color assers & desain first page menu sidebar

--- src/pages/ResepMakanan.tsx
+++ src/pages/ResepMakanan.tsx
@@ -1,9 +1,34 @@
+import { Link } from "react-router-dom";
 import MainLayout from "../layout/Mainlayout";
 
 const ResepMakanan = () => {
   return (
     <MainLayout>
-      <div className="text-orange text-2xl">ResepMakanan</div>;
+      <section className="ml-[20px] mt-[35px] mr-[20px]">
+        <h1 className="heading1 mb-5">Menu Upload Resep Makanan & Artikel</h1>
+        <div className="my-3 py-2 px-4 border border-border-grey rounded-lg flex flex-col sm:flex-row justify-between items-center">
+          <p className="mb-2 sm:mb-0 sm:mr-4">Menu Resep Makanan</p>
+          <Link to={"#"}>
+            <button
+              type="button"
+              className="text-light-violet outline-none mt-2 sm:mt-0"
+            >
+              Lihat Menu
+            </button>
+          </Link>
+        </div>
+        <div className="my-3 py-2 px-4 border border-border-grey rounded-lg flex flex-col sm:flex-row justify-between items-center">
+          <p className="mb-2 sm:mb-0 sm:mr-4">Menu Artikel</p>
+          <Link to={"#"}>
+            <button
+              type="button"
+              className="text-light-violet outline-none mt-2 sm:mt-0"
+            >
+              Lihat Menu
+            </button>
+          </Link>
+        </div>
+      </section>
     </MainLayout>
   );
 };
